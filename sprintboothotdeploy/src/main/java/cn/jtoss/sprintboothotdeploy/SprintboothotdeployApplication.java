6b50506da6ab43f8fcd3ca50d531ef4b5700f70a
huanghengwen <hengweno@163.com>
2023-04-20 18:53:55 +0800

    springboot hot deploy

---
+++ sprintboothotdeploy/src/main/java/cn/jtoss/sprintboothotdeploy/SprintboothotdeployApplication.java
@@ -0,0 +1,13 @@
+package cn.jtoss.sprintboothotdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintboothotdeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintboothotdeployApplication.class, args);
+	}
+
+}
---
+++ sprintboothotdeploy/src/main/java/cn/jtoss/sprintboothotdeploy/controller/DemoController.java
@@ -0,0 +1,16 @@
+package cn.jtoss.sprintboothotdeploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jason
+ */
+@RestController
+public class DemoController {
+    @GetMapping("/demo")
+    public String demo() {
+        String message = "hello world";
+        return message;
+    }
+}
