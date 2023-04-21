0fbdfd4967c651d0ce7e5022511c20d90c78f3bd
huanghengwen <hengweno@163.com>
2023-04-21 16:26:58 +0800

    @InitBinder

--- springbootvalidation/src/main/java/cn/jtoss/springbootvalidation/controller/DemoController.java
+++ springbootvalidation/src/main/java/cn/jtoss/springbootvalidation/controller/DemoController.java
@@ -4,11 +4,13 @@ import cn.jtoss.springbootvalidation.domain.ValidVO;
 import cn.jtoss.springbootvalidation.validator.ValidGroup;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Email;
+import java.util.Date;
 
 /**
  * @author jason
@@ -45,5 +47,10 @@ public class DemoController {
     public String update(@Validated(value = ValidGroup.Crud.Update.class) ValidVO validVO){
         log.info("validEntity is {}", validVO);
         return "test4 valid success";
+    }
+
+    @GetMapping("testDate")
+    public Date processApi(Date date) {
+        return date;
     }
 }
--- springbootvalidation/src/main/java/cn/jtoss/springbootvalidation/response/ResponseAdvice.java
+++ springbootvalidation/src/main/java/cn/jtoss/springbootvalidation/response/ResponseAdvice.java
@@ -4,13 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author jason
@@ -37,5 +43,11 @@ public class ResponseAdvice implements ResponseBodyAdvice<Object> {
         }
 
         return ResultResponse.createSuccessResponse(object);
+    }
+
+    @InitBinder
+    public void handleInitBinder(WebDataBinder dataBinder){
+        dataBinder.registerCustomEditor(Date.class,
+                new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), false));
     }
 }
